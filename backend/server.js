c435d62b789a5742dcd4126772af9479f68acaa4
Michael <mtpanlilio20@hotmail.com>
2024-10-24 12:47:36 +0100

    Created APIs to request IDs and display the correct chats

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ app.get('/', (req,res) => {
     res.send("API is Running");
 })
 
+app.get('/api/chat', (req,res) => {
+    res.send()
+})
+
+app.get('/api/chat/:id', (req,res) => {
+    // console.log(req);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
+
 app.listen(5000, () => {
     console.log("Server Started on PORT 5000");
 });
