d83d5669370c780d398fe3034b307013f2af6db0
Michael <mtpanlilio20@hotmail.com>
2024-10-24 12:37:55 +0100

    Refactored code with missing ()

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
-const app = express;
+const app = express();
 
-app.listen(5000, console.log("Server Started on PORT 5000"));
+app.listen(5000, () => {
+    console.log("Server Started on PORT 5000");
+});
